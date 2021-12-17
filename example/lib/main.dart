0ae22f2b87dd81566e079e356c855c9a69c85f3b
Divyanshu Shekhar <imdshekhar@gmail.com>
2021-12-17 11:36:58 +0530

    example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,20 +1,60 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:reflex/reflex.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  Reflex? _reflex;
+  StreamSubscription<NotificationEvent>? _subscription;
+  final List<NotificationEvent> _log = [];
+  bool started = false;
+
   @override
   void initState() {
     super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    startListening();
+  }
+
+  void onData(NotificationEvent event) {
+    setState(() {
+      _log.add(event);
+    });
+    debugPrint(event.toString());
+  }
+
+  void startListening() {
+    _reflex = Reflex();
+    try {
+      _subscription = _reflex!.notificationStream!.listen(onData);
+      setState(() {
+        started = true;
+      });
+    } on ReflexException catch (exception) {
+      debugPrint(exception.toString());
+    }
+  }
+
+  void stopListening() {
+    _subscription?.cancel();
+    setState(() => started = false);
   }
 
   @override
@@ -22,10 +62,26 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Reflex Example app'),
         ),
-        body: const Center(
-          child: Text('App Running!!\n'),
+        body: Center(
+          child: ListView.builder(
+            itemCount: _log.length,
+            itemBuilder: (BuildContext context, int idx) {
+              final entry = _log[idx];
+              return ListTile(
+                title: Text(entry.title ?? ""),
+                subtitle: Text(entry.message ?? ""),
+                trailing: Text(entry.packageName.toString().split('.').last),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: started ? stopListening : startListening,
+          tooltip: 'Start/Stop listening',
+          child:
+              started ? const Icon(Icons.stop) : const Icon(Icons.play_arrow),
         ),
       ),
     );
